d88b91b9e278743ba46b96c674039444d2b080b3
kentre <kentrep@bellsouth.net>
2024-02-17 18:10:02 -0500

    "views hard code index.js"

--- Website/assets/js/index.js
+++ Website/assets/js/index.js
@@ -4,7 +4,10 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://5ujlilof3karm4gau65gddysr40pxdub.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = ' Views: ${data}';
+    counter.innerHTML = ' Views: 137';
 }
 
 updateCounter();
+
+
+#${data} figure out issue on line7
